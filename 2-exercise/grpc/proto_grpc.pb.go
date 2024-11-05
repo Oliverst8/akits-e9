a682bdbd2adbb9c078b939ad492d123c8eba6631
osta <osta@itu.dk>
2024-11-05 11:59:00 +0100

    Made it so it should work but no reqeusts are made yet

--- 2-exercise/grpc/proto_grpc.pb.go
+++ 2-exercise/grpc/proto_grpc.pb.go
@@ -19,9 +19,10 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	MutexNode_Join_FullMethodName    = "/MutexNode/Join"
-	MutexNode_Request_FullMethodName = "/MutexNode/Request"
-	MutexNode_AddNode_FullMethodName = "/MutexNode/AddNode"
+	MutexNode_Join_FullMethodName             = "/MutexNode/Join"
+	MutexNode_Request_FullMethodName          = "/MutexNode/Request"
+	MutexNode_RespondToRequest_FullMethodName = "/MutexNode/RespondToRequest"
+	MutexNode_AddNode_FullMethodName          = "/MutexNode/AddNode"
 )
 
 // MutexNodeClient is the client API for MutexNode service.
@@ -29,7 +30,8 @@ const (
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type MutexNodeClient interface {
 	Join(ctx context.Context, in *JoinMessage, opts ...grpc.CallOption) (*JoinResponse, error)
-	Request(ctx context.Context, in *RequestMessage, opts ...grpc.CallOption) (*Reply, error)
+	Request(ctx context.Context, in *RequestMessage, opts ...grpc.CallOption) (*Empty, error)
+	RespondToRequest(ctx context.Context, in *Reply, opts ...grpc.CallOption) (*Empty, error)
 	AddNode(ctx context.Context, in *JoinMessage, opts ...grpc.CallOption) (*JoinMessage, error)
 }
 
@@ -51,10 +53,20 @@ func (c *mutexNodeClient) Join(ctx context.Context, in *JoinMessage, opts ...grp
 	return out, nil
 }
 
-func (c *mutexNodeClient) Request(ctx context.Context, in *RequestMessage, opts ...grpc.CallOption) (*Reply, error) {
+func (c *mutexNodeClient) Request(ctx context.Context, in *RequestMessage, opts ...grpc.CallOption) (*Empty, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(Reply)
+	out := new(Empty)
 	err := c.cc.Invoke(ctx, MutexNode_Request_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *mutexNodeClient) RespondToRequest(ctx context.Context, in *Reply, opts ...grpc.CallOption) (*Empty, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(Empty)
+	err := c.cc.Invoke(ctx, MutexNode_RespondToRequest_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
@@ -76,7 +88,8 @@ func (c *mutexNodeClient) AddNode(ctx context.Context, in *JoinMessage, opts ...
 // for forward compatibility.
 type MutexNodeServer interface {
 	Join(context.Context, *JoinMessage) (*JoinResponse, error)
-	Request(context.Context, *RequestMessage) (*Reply, error)
+	Request(context.Context, *RequestMessage) (*Empty, error)
+	RespondToRequest(context.Context, *Reply) (*Empty, error)
 	AddNode(context.Context, *JoinMessage) (*JoinMessage, error)
 	mustEmbedUnimplementedMutexNodeServer()
 }
@@ -91,8 +104,11 @@ type UnimplementedMutexNodeServer struct{}
 func (UnimplementedMutexNodeServer) Join(context.Context, *JoinMessage) (*JoinResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Join not implemented")
 }
-func (UnimplementedMutexNodeServer) Request(context.Context, *RequestMessage) (*Reply, error) {
+func (UnimplementedMutexNodeServer) Request(context.Context, *RequestMessage) (*Empty, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Request not implemented")
+}
+func (UnimplementedMutexNodeServer) RespondToRequest(context.Context, *Reply) (*Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RespondToRequest not implemented")
 }
 func (UnimplementedMutexNodeServer) AddNode(context.Context, *JoinMessage) (*JoinMessage, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddNode not implemented")
@@ -154,6 +170,24 @@ func _MutexNode_Request_Handler(srv interface{}, ctx context.Context, dec func(i
 	return interceptor(ctx, in, info, handler)
 }
 
+func _MutexNode_RespondToRequest_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(Reply)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MutexNodeServer).RespondToRequest(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: MutexNode_RespondToRequest_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MutexNodeServer).RespondToRequest(ctx, req.(*Reply))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _MutexNode_AddNode_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(JoinMessage)
 	if err := dec(in); err != nil {
@@ -186,6 +220,10 @@ var MutexNode_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Request",
 			Handler:    _MutexNode_Request_Handler,
+		},
+		{
+			MethodName: "RespondToRequest",
+			Handler:    _MutexNode_RespondToRequest_Handler,
 		},
 		{
 			MethodName: "AddNode",
--- 2-exercise/main.go
+++ 2-exercise/main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	proto "main/grpc"
 	"net"
 	"os"
+	"strconv"
 	"sync"
 )
-
-var lock sync.Mutex
 
 // MutexNode Each node has a server to receive information and a list of clients to send information to all other nodes
 type MutexNode struct {
 	proto.UnimplementedMutexNodeServer
 	port        string
-	clients     []proto.MutexNodeClient
+	clients     map[string]proto.MutexNodeClient
 	state       string
-	myRequests  []proto.RequestMessage
+	myRequests  []proto.MutexNodeClient
 	lamportTime uint64
+	responses   int
 }
 
-func main() {
-	clientPort := os.Args[1] //The port of this node
+var responsesLock sync.Mutex
+var reqeustLock sync.Mutex
 
+func main() {
+	clientPort := os.Args[2] //The port of this node
+	desiredNetworkSize, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
 	node := &MutexNode{}
 
 	node.start_server(clientPort) //Starting a server so that this node listen on the given port
 
-	if len(os.Args) > 2 { //If the client isnt the starting node
+	if len(os.Args) > 3 { //If the client isnt the starting node
 
-		joinOn := os.Args[2] //The port to send the join request to
+		joinOn := os.Args[3] //The port to send the join request to
 
 		node.start_client(joinOn)
 
@@ -39,7 +47,7 @@ func main() {
 			Port: clientPort,
 		}
 
-		response, err := node.clients[0].Join(context.Background(), &message) //Trying to join using the first node
+		response, err := node.clients[joinOn].Join(context.Background(), &message) //Trying to join using the first node
 
 		if err != nil {
 			panic(err)
@@ -53,13 +61,26 @@ func main() {
 		for _, port := range response.Ports {
 			node.start_client(port)
 		}
-		node.state = "RELEASED"
+
 	}
+	for len(node.clients) < desiredNetworkSize {
+	}
+	node.state = "RELEASED"
 	for {
 		if node.state == "RELEASED" {
-			panic("Not implemented")
+			reqeustLock.Lock()
+			for _, client := range node.myRequests {
+				reply := proto.Reply{
+					Success: true,
+					Time:    node.lamportTime,
+				}
+				_, err = client.RespondToRequest(context.Background(), &reply)
+				if err != nil {
+					panic(err)
+				}
+			}
+			reqeustLock.Unlock()
 		}
-
 	}
 }
 
@@ -83,16 +104,13 @@ func (s MutexNode) start_client(port string) { // start up a new client for the 
 	if err != nil {
 		panic(err)
 	}
-	lock.Lock()
-	s.clients = append(s.clients, proto.NewMutexNodeClient(conn))
-	lock.Unlock()
+	s.clients[port] = proto.NewMutexNodeClient(conn)
 }
 
 // Join Called from client, to make a request to join the network
 func (s MutexNode) Join(context context.Context, message *proto.JoinMessage) (*proto.JoinResponse, error) {
 	//Sends a
 	var ports []string
-	lock.Lock()
 	for _, client := range s.clients {
 		res, err := client.AddNode(context, message)
 		if err != nil {
@@ -100,7 +118,6 @@ func (s MutexNode) Join(context context.Context, message *proto.JoinMessage) (*p
 		}
 		ports = append(ports, res.Port)
 	}
-	lock.Unlock()
 	reply := proto.JoinResponse{
 		Ports:   ports,
 		Success: true,
@@ -118,17 +135,100 @@ func (s MutexNode) AddNode(context context.Context, message *proto.JoinMessage) 
 	}, nil
 }
 
-func (s MutexNode) Request(context context.Context, message *proto.RequestMessage) (*proto.Reply, error) {
-
-}
-
-func (s MutexNode) makeRequest(message *proto.RequestMessage) {
-	lock.Lock()
-	for _, client := range s.clients {
-		_, err := client.Request(context.Background(), message)
+func (s MutexNode) Request(context context.Context, message *proto.RequestMessage) (*proto.Empty, error) {
+	requestingClient := s.clients[message.Port]
+	if s.state == "HELD" || (s.state == "WANTED" && s.compare(message)) {
+		s.myRequests = append(s.myRequests, requestingClient)
+	} else {
+		reply := proto.Reply{
+			Success: true,
+			Time:    s.lamportTime,
+		}
+		_, err := requestingClient.RespondToRequest(context, &reply)
 		if err != nil {
 			panic(err)
 		}
 	}
-	lock.Unlock()
+	return &proto.Empty{}, nil
+}
+
+func (s MutexNode) RespondToRequest(context context.Context, reply *proto.Reply) (*proto.Empty, error) {
+	responsesLock.Lock()
+	s.responses += 1
+	responsesLock.Unlock()
+	return &proto.Empty{}, nil
+}
+
+func (s MutexNode) compare(message *proto.RequestMessage) bool {
+	thisPort, err := strconv.Atoi(s.port)
+	if err != nil {
+		panic(err)
+	}
+	thatPort, err := strconv.Atoi(message.Port)
+	if err != nil {
+		panic(err)
+	}
+	if s.lamportTime < message.Time {
+		return true
+	}
+	if s.lamportTime == message.Time && thisPort < thatPort {
+		return true
+	}
+	return false
+}
+
+func (s MutexNode) multicast(message *proto.RequestMessage) {
+	s.state = "WANTED"
+	s.responses = 0
+	for _, client := range s.clients {
+		go makeRequest(client, message)
+	}
+	for {
+		if s.responses >= len(s.clients) {
+			break
+		}
+	}
+	s.state = "HELD"
+	incrementFile()
+	s.state = "RELEASED"
+}
+
+func incrementFile() {
+	file, err := os.OpenFile("log.txt", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0600)
+	if err != nil {
+		if os.IsNotExist(err) {
+			file, err = os.Create("log.txt")
+			if err != nil {
+				panic(err)
+			}
+		} else {
+			panic(err)
+		}
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	text := "0"
+	for scanner.Scan() {
+		text = scanner.Text()
+	}
+
+	newNumber, err := strconv.Atoi(text)
+	if err != nil {
+		panic(err)
+	}
+
+	newNumber++
+
+	_, err = fmt.Fprintf(file, "%d\n", newNumber)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func makeRequest(client proto.MutexNodeClient, message *proto.RequestMessage) {
+	_, err := client.Request(context.Background(), message)
+	if err != nil {
+		panic(err)
+	}
 }
